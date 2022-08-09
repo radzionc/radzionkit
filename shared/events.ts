cecff5053d0f8d5dab07e6c4e56488fe5fcd63fe
Rodion Chachura <geekrodion@gmail.com>
2022-08-09 13:58:03 +0400

    remove lodash deps

--- shared/events.ts
+++ shared/events.ts
@@ -1,27 +1,34 @@
-import { flow } from "lodash"
-
 interface AnyEvent {
-  stopPropagation: () => void
-  preventDefault: () => void
+  stopPropagation: () => void;
+  preventDefault: () => void;
 }
 
 export function handleWithStopPropagation<E extends AnyEvent>(
   handler?: (event?: E) => void
 ) {
   return function handle(event?: E) {
-    event?.stopPropagation?.()
+    event?.stopPropagation?.();
 
-    handler?.(event)
-  }
+    handler?.(event);
+  };
 }
 
 export function handleWithPreventDefault<E extends AnyEvent>(
   handler?: (event: E) => void
 ) {
   return function handle(event: E) {
-    event?.preventDefault?.()
-    handler?.(event)
-  }
+    event?.preventDefault?.();
+    handler?.(event);
+  };
 }
 
-export const handleWithStopPropagationAndPreventDefault = flow(handleWithPreventDefault, handleWithStopPropagation)
+export function handleWithStopPropagationAndPreventDefault<E extends AnyEvent>(
+  handler?: (event: E) => void
+) {
+  return function handle(event: E) {
+    event?.preventDefault?.();
+    event?.stopPropagation?.();
+
+    handler?.(event);
+  };
+}
