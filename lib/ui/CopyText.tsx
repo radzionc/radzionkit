4a5a6fb41b884c50ab2f017a9ca817691824bd92
Rodion Chachura <geekrodion@gmail.com>
2023-06-30 16:25:59 +0400

    feat: use hover effect for copy text

--- lib/ui/CopyText.tsx
+++ lib/ui/CopyText.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components"
 import { getColor } from "./theme/getters"
 import copy from "copy-to-clipboard"
 import { defaultTransitionCSS } from "./animations/transitions"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { Match } from "./Match"
 import { CheckIcon } from "./icons/CheckIcon"
 
@@ -31,20 +31,10 @@ type IconToShow = "copy" | "copied"
 export const CopyText = ({ content, children, ...rest }: CopyTextProps) => {
   const [iconToShow, setIconToShow] = useState<IconToShow>("copy")
 
-  useEffect(() => {
-    if (iconToShow === "copy") return
-
-    const timeout = setTimeout(() => {
-      setIconToShow("copy")
-    }, 1000)
-
-    return () => {
-      clearTimeout(timeout)
-    }
-  }, [iconToShow])
-
   return (
     <Container
+      onMouseLeave={() => setIconToShow("copy")}
+      onTouchEnd={() => setIconToShow("copy")}
       onClick={() => {
         copy(content)
         setIconToShow("copied")
