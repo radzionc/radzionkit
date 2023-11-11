fa9ef061c9091bf8fefb57372e35836768590bd8
Rodion Chachura <geekrodion@gmail.com>
2023-11-11 12:21:55 +0400

    Delete unused code related to Combobox and LanguagesInput components

--- app/pages/combobox.tsx
+++ app/pages/combobox.tsx
@@ -1,93 +1,28 @@
-import { Form } from '@reactkit/ui/forms/Form'
-import { TextInput } from '@reactkit/ui/inputs/TextInput'
-import { Controller, useForm } from 'react-hook-form'
-import * as yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { LanguagesInput } from 'components/LanguagesInput'
+import { CountryCode } from '@reactkit/utils/countries'
 import { DemoPage } from 'components/DemoPage'
-import { Panel } from '@reactkit/ui/panel/Panel'
-import { FixedOptionsInput } from '@reactkit/ui/inputs/Combobox/FixedOptionsInput'
-import { capitalizeFirstLetter } from '@reactkit/utils/capitalizeFirstLetter'
-import { Button } from '@reactkit/ui/buttons/Button'
 import { makeDemoPage } from 'layout/makeDemoPage'
-import { TitledSection } from '@reactkit/ui/layout/TitledSection'
+import { useState } from 'react'
+import { CountryInput } from '@reactkit/ui/inputs/CountryInput'
+import styled from 'styled-components'
 
-interface FormShape {
-  name: string
-  languages: string[]
-}
-
-const schema: yup.SchemaOf<FormShape> = yup
-  .object({
-    name: yup.string().max(100).required(),
-    languages: yup.array().min(1),
-  })
-  .required()
+const Container = styled.div`
+  max-width: 360px;
+  min-width: 320px;
+  width: 100%;
+`
 
 export default makeDemoPage(() => {
-  const {
-    handleSubmit,
-    register,
-    control,
-    formState: { errors },
-  } = useForm<FormShape>({
-    mode: 'onSubmit',
-    resolver: yupResolver(schema),
-    defaultValues: {
-      languages: [],
-    },
-  })
+  const [value, setValue] = useState<CountryCode | undefined>('PT')
 
   return (
-    <DemoPage title="Combobox" youtubeVideoId="iZGQE3-pqpg">
-      <Panel width={400}>
-        <TitledSection title="Who are You?">
-          <Form
-            content={
-              <>
-                <TextInput
-                  label="Full name"
-                  {...register('name')}
-                  error={errors.name?.message}
-                  autoFocus
-                  placeholder="John Johnson"
-                />
-                <Controller
-                  control={control}
-                  name="languages"
-                  render={({ field: { value, onChange, ref } }) => (
-                    <LanguagesInput
-                      value={value}
-                      onChange={onChange}
-                      ref={ref}
-                      error={errors.languages?.message}
-                    />
-                  )}
-                />
-                <FixedOptionsInput
-                  label="Loading example"
-                  isLoading
-                  placeholder="Languages"
-                  value={null}
-                  onChange={() => {}}
-                  options={['one', 'two', 'three']}
-                  optionToString={capitalizeFirstLetter}
-                />
-                <FixedOptionsInput
-                  label="No options example"
-                  placeholder="Languages"
-                  value={null}
-                  onChange={() => {}}
-                  options={[]}
-                  optionToString={capitalizeFirstLetter}
-                />
-              </>
-            }
-            onSubmit={handleSubmit(console.log)}
-            actions={<Button size="l">Submit</Button>}
-          />
-        </TitledSection>
-      </Panel>
+    <DemoPage title="Combobox">
+      <Container>
+        <CountryInput
+          label="Country of residence"
+          value={value}
+          onChange={setValue}
+        />
+      </Container>
     </DemoPage>
   )
 })
