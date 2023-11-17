5b8a1d7f34bf214d7e47a4146fa1c43e8ba5b2f2
Rodion Chachura <geekrodion@gmail.com>
2023-11-17 14:14:53 +0400

    Add min-width and min-height to IconWrapper component
    Add extractTemplateVariables utility function

--- ui/icons/IconWrapper.tsx
+++ ui/icons/IconWrapper.tsx
@@ -8,4 +8,6 @@ export const IconWrapper = styled.span`
   width: fit-content;
   height: fit-content;
   overflow: hidden;
+  min-width: 1em;
+  min-height: 1em;
 `
---
+++ utils/extractTemplateVariables.ts
@@ -0,0 +1,9 @@
+import { withoutDuplicates } from './array/withoutDuplicates'
+
+export const extractTemplateVariables = (str: string): string[] => {
+  const variableRegex = /\{\{(\w+)\}\}/g
+  const matches = str.match(variableRegex)
+  if (!matches) return []
+
+  return withoutDuplicates(matches.map((match) => match.slice(2, -2)))
+}
