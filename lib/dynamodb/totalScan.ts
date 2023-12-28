037d04356e25c69ea3728fd3945cc39a810d3f1b
Rodion Chachura <geekrodion@gmail.com>
2023-12-28 13:53:35 +0400

    Add syncpack to package.json scripts

--- lib/dynamodb/totalScan.ts
+++ lib/dynamodb/totalScan.ts
@@ -1,17 +1,13 @@
-import {
-  DynamoDBDocumentClient,
-  ScanCommand,
-  ScanCommandInput,
-} from '@aws-sdk/lib-dynamodb'
+import { ScanCommand, ScanCommandInput } from '@aws-sdk/lib-dynamodb'
 import { fetchAll } from '@lib/utils/query/fetchAll'
+import { dbDocClient } from './client'
 
 export const totalScan = <T>(
-  client: DynamoDBDocumentClient,
   params: Omit<ScanCommandInput, 'ExclusiveStartKey'>,
 ): Promise<T[]> => {
   return fetchAll({
     fetch: (lastEvaluatedKey: ScanCommandInput['ExclusiveStartKey']) => {
-      return client.send(
+      return dbDocClient.send(
         new ScanCommand({
           ExclusiveStartKey: lastEvaluatedKey,
           ...params,
