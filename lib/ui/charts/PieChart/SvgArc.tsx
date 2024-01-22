1c7827cef46fd43c13aa40ed80acb1beff0f07b9
Rodion Chachura <geekrodion@gmail.com>
2024-01-22 15:11:30 +0400

    Add support for rendering a complete circle in SvgArc component

--- lib/ui/charts/PieChart/SvgArc.tsx
+++ lib/ui/charts/PieChart/SvgArc.tsx
@@ -76,6 +76,60 @@ export const SvgArc = ({
   radius,
   cutoutRadius,
 }: Props) => {
+  if (endAngle - startAngle >= 360) {
+    const largeArcFlag = 0
+    const outerStart = polarToCartesian(radius, radius, 0)
+    const outerMid = polarToCartesian(radius, radius, 180)
+    const outerEnd = polarToCartesian(radius, radius, 360)
+    const innerStart = polarToCartesian(radius, cutoutRadius, 0)
+    const innerMid = polarToCartesian(radius, cutoutRadius, 180)
+    const innerEnd = polarToCartesian(radius, cutoutRadius, 360)
+
+    const path = [
+      'M',
+      outerStart.x,
+      outerStart.y,
+      'A',
+      radius,
+      radius,
+      0,
+      largeArcFlag,
+      1,
+      outerMid.x,
+      outerMid.y,
+      'A',
+      radius,
+      radius,
+      0,
+      largeArcFlag,
+      1,
+      outerEnd.x,
+      outerEnd.y,
+      'L',
+      innerEnd.x,
+      innerEnd.y,
+      'A',
+      cutoutRadius,
+      cutoutRadius,
+      0,
+      largeArcFlag,
+      0,
+      innerMid.x,
+      innerMid.y,
+      'A',
+      cutoutRadius,
+      cutoutRadius,
+      0,
+      largeArcFlag,
+      0,
+      innerStart.x,
+      innerStart.y,
+      'Z',
+    ].join(' ')
+
+    return <path fill={color.toCssValue()} d={path} />
+  }
+
   const path = getArcPath(radius, cutoutRadius, startAngle, endAngle)
 
   return <path fillRule="evenodd" fill={color.toCssValue()} d={path} />
