5f789f88a8a63709e5762255479e9ea0cc09264b
Rodion Chachura <geekrodion@gmail.com>
2024-07-20 11:47:32 +0400

    refactor: Add EmbeddedDescriptionInput and EmbeddedTitleInput components

---
+++ lib/ui/inputs/EmbeddedDescriptionInput.tsx
@@ -0,0 +1,63 @@
+import { toSizeUnit } from '@lib/ui/css/toSizeUnit'
+import { FormSectionShyTitle } from '@lib/ui/form/components/FormSectionShyTitle'
+import { VStack } from '@lib/ui/layout/Stack'
+import { InputProps } from '@lib/ui/props'
+import { ComponentProps, ReactNode, useRef } from 'react'
+import styled from 'styled-components'
+import { MultilineTextInput } from './MultilineTextInput'
+import { tightListItemConfig } from '../list/tightListItemConfig'
+
+const padding = 20
+
+const Wrapper = styled(VStack)`
+  padding: 0;
+  position: relative;
+  > * {
+    padding: ${toSizeUnit(padding)};
+
+    &:last-child {
+      padding-top: ${toSizeUnit(padding + 24)};
+    }
+  }
+`
+
+const PositionLabel = styled.div`
+  position: absolute;
+  pointer-events: none;
+`
+
+const Container = styled(MultilineTextInput)`
+  line-height: ${toSizeUnit(tightListItemConfig.lineHeight)};
+  background: transparent;
+  width: 100%;
+  font-size: 14px;
+`
+
+type EmbeddedDescriptionInputProps = InputProps<string> &
+  Omit<ComponentProps<typeof Container>, 'value' | 'onChange'> & {
+    label: ReactNode
+  }
+
+export const EmbeddedDescriptionInput = ({
+  value,
+  onChange,
+  label,
+  ...rest
+}: EmbeddedDescriptionInputProps) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+
+  return (
+    <Wrapper>
+      <PositionLabel>
+        <FormSectionShyTitle>{label}</FormSectionShyTitle>
+      </PositionLabel>
+      <Container
+        autoComplete="off"
+        ref={textareaRef}
+        value={value}
+        onChange={onChange}
+        {...rest}
+      />
+    </Wrapper>
+  )
+}
---
+++ lib/ui/inputs/EmbeddedTitleInput.tsx
@@ -0,0 +1,48 @@
+import { toSizeUnit } from '@lib/ui/css/toSizeUnit'
+import { InputProps } from '@lib/ui/props'
+import { ComponentProps, forwardRef } from 'react'
+import styled from 'styled-components'
+
+import { MultilineTextInput } from '@lib/ui/inputs/MultilineTextInput'
+import { tightListItemConfig } from '@lib/ui/list/tightListItemConfig'
+
+const Container = styled(MultilineTextInput)`
+  line-height: ${toSizeUnit(tightListItemConfig.lineHeight)};
+  background: transparent;
+  width: 100%;
+  font-size: 16px;
+  font-weight: 500;
+`
+
+type EmbeddedTitleInputProps = InputProps<string> &
+  Omit<ComponentProps<typeof Container>, 'value' | 'onChange'> & {
+    lineBreakEnabled?: boolean
+    onSubmit?: () => void
+  }
+
+export const EmbeddedTitleInput = forwardRef<
+  HTMLTextAreaElement,
+  EmbeddedTitleInputProps
+>(({ value, onChange, onSubmit, lineBreakEnabled, ...rest }, ref) => {
+  return (
+    <Container
+      value={value}
+      onChange={onChange}
+      autoComplete="off"
+      onKeyDown={(event) => {
+        if (event.key !== 'Enter') return
+
+        if (!event.shiftKey) {
+          event.preventDefault()
+          onSubmit?.()
+        }
+
+        if (!lineBreakEnabled) {
+          event.preventDefault()
+        }
+      }}
+      ref={ref}
+      {...rest}
+    />
+  )
+})
