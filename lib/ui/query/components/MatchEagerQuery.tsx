80c98a4249700ddbfff22c1a49ee963ed6900a46
Rodion Chachura <geekrodion@gmail.com>
2024-12-05 08:26:47 +0400

    Remove EagerQueryDependant and QueryDependant components, and introduce MatchEagerQuery and MatchQuery components for improved query handling and consistency

---
+++ lib/ui/query/components/MatchEagerQuery.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react'
+import { EagerQuery } from '../Query'
+import { ComponentWithValueProps } from '../../props'
+
+export type MatchEagerQueryProps<T, E = unknown> = ComponentWithValueProps<
+  EagerQuery<T, E>
+> & {
+  pending: () => ReactNode
+  error: (errors: E[]) => ReactNode
+  success: (data: T) => ReactNode
+}
+
+export function MatchEagerQuery<T, E = unknown>({
+  value: { data, isPending, errors },
+  error,
+  pending,
+  success,
+}: MatchEagerQueryProps<T, E>) {
+  if (data !== undefined) {
+    return <>{success(data)}</>
+  }
+
+  if (isPending) {
+    return <>{pending()}</>
+  }
+
+  if (errors.length > 0) {
+    return <>{error(errors)}</>
+  }
+
+  return null
+}
+
+export type MatchEagerQueryWrapperProps<T> = Pick<
+  MatchEagerQueryProps<T>,
+  'success'
+> &
+  Partial<Pick<MatchEagerQueryProps<T>, 'error' | 'pending'>>
---
+++ lib/ui/query/components/MatchQuery.tsx
@@ -0,0 +1,38 @@
+import { ComponentWithValueProps } from '../../props'
+import { Query } from '../Query'
+import { ReactNode } from 'react'
+
+export type MatchQueryProps<T, E = unknown> = ComponentWithValueProps<
+  Query<T, E>
+> & {
+  error: (error: E) => ReactNode
+  pending: () => ReactNode
+  success: (data: T) => ReactNode
+}
+
+export function MatchQuery<T, E = unknown>({
+  value,
+  error,
+  pending,
+  success,
+}: MatchQueryProps<T, E>) {
+  if (value.data !== undefined) {
+    return <>{success(value.data)}</>
+  }
+
+  if (value.isPending) {
+    return <>{pending()}</>
+  }
+
+  if (value.error) {
+    return <>{error(value.error)}</>
+  }
+
+  return null
+}
+
+export type MatchQueryWrapperProps<T, E = unknown> = Pick<
+  MatchQueryProps<T>,
+  'success'
+> &
+  Partial<Pick<MatchQueryProps<T, E>, 'error' | 'pending'>>
