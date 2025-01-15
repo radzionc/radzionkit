93e04cf94fed53c5de94fde4545f601507c91b5c
Rodion Chachura <geekrodion@gmail.com>
2025-01-15 08:11:36 +0400

    Update README to include new tutorial link for 0x Swap API with AWS Lambda, and refactor recordMap utility to accept key as an argument for enhanced functionality.

--- lib/utils/record/recordMap.ts
+++ lib/utils/record/recordMap.ts
@@ -1,8 +1,11 @@
 export const recordMap = <K extends string | number, T, V>(
   record: Record<K, T>,
-  fn: (value: T) => V,
+  fn: (value: T, key: K) => V,
 ): Record<K, V> => {
   return Object.fromEntries(
-    Object.entries(record).map(([key, value]) => [key, fn(value as T)]),
+    Object.entries(record).map(([key, value]) => [
+      key,
+      fn(value as T, key as K),
+    ]),
   ) as Record<K, V>
 }
