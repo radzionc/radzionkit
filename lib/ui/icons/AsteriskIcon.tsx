c0f2438d7bdd007b8151d9241031897d9e207fa5
Rodion Chachura <geekrodion@gmail.com>
2025-02-10 08:11:49 +0400

    Add new SVG icons for Asterisk, Bitcoin, and Chart Candlestick
    - Create AsteriskIcon component with SVG path
    - Implement BitcoinIcon component with cryptocurrency symbol
    - Add ChartCandleStickIcon for chart visualization
    - Include general coding rules for pattern matching and component usage

---
+++ lib/ui/icons/AsteriskIcon.tsx
@@ -0,0 +1,20 @@
+import { SvgIconProps } from './SvgIconProps'
+
+export const AsteriskIcon = (props: SvgIconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M12 6v12" />
+    <path d="M17.196 9 6.804 15" />
+    <path d="m6.804 9 10.392 6" />
+  </svg>
+)
