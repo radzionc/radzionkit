70623f99a8a1194a136a6283b12b8b7525fce8cd
Rodion Chachura <geekrodion@gmail.com>
2025-03-18 14:27:32 +0400

    Refactor GroupedRadioInput and InputContainer styles for improved layout
    - Adjust the GroupedRadioInput component to enable flex-wrap and set a minimum height for better responsiveness.
    - Remove the fixed width from the InputContainer to allow for more flexible layouts.

--- lib/ui/inputs/GroupedRadioInput.tsx
+++ lib/ui/inputs/GroupedRadioInput.tsx
@@ -17,15 +17,16 @@ type RadioInputProps<T extends string> = InputProps<T> &
   }
 
 const Wrapper = styled(HStack)`
-  height: 40px;
   gap: 2px;
   padding: 2px;
   ${borderRadius.m};
   border: 2px solid ${getColor('mistExtra')};
+  flex-wrap: wrap;
 `
 
 const Container = styled.label<IsActiveProp>`
   position: relative;
+  min-height: 32px;
   ${horizontalPadding(12)};
   ${borderRadius.s};
   display: flex;
--- lib/ui/inputs/InputContainer.tsx
+++ lib/ui/inputs/InputContainer.tsx
@@ -3,7 +3,6 @@ import styled, { css } from 'styled-components'
 export const inputContainer = css`
   display: flex;
   flex-direction: column;
-  width: 100%;
   gap: 8px;
 `
 
